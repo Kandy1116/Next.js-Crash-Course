35b55c08724630eced88ec92317837d342d6777a
Kandy1116 <venus.kandy@gmail.com>
2026-02-05 15:15:41 -0600

    update work

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 
+// fetch from API: https://jsonplaceholder.typicode.com/posts
 export async function getServerSideProps() {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts") 
     const data = await res.json()
 
     return {
